286d00d36beec1422c6033b238d1b45a6376aa00
YASIN AKCA <yasin.akca@bil.omu.edu.tr>
2020-03-31 14:01:13 +0300

    ios face recognition uygulamaları

--- FaceRecognition/ViewController.swift
+++ FaceRecognition/ViewController.swift
@@ -7,14 +7,37 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func signInClicked(_ sender: Any) {
+        
+        let authContext = LAContext()
+        
+        var error: NSError?
+        
+        if authContext.canEvaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            authContext.evaluatePolicy(LAPolicy.deviceOwnerAuthenticationWithBiometrics, localizedReason: "Is it you?") { (success, error) in
+                if success == true {
+                    DispatchQueue.main.async {
+                        self.performSegue(withIdentifier: "toSecondVC", sender: nil)
+                    }
+                }else {
+                    DispatchQueue.main.async {
+                        self.label.text = "Error!"
+                    }
+                }
+            }
+        }
+    }
+    
 }
 
