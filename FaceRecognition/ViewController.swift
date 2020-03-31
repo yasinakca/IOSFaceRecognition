6a9b961a66274575c8032de24c6b02cc313d2130
YASIN AKCA <yasin.akca@bil.omu.edu.tr>
2020-03-31 13:47:57 +0300

    Initial Commit

---
+++ FaceRecognition/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FaceRecognition
+//
+//  Created by YASIN AKCA on 31.03.2020.
+//  Copyright © 2020 YASIN AKCA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
